bcdf575925231175d3a81cbc259fb9073299d11d
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-01-15 23:26:54 +0100

    fixed user index page and authentication error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,26 @@
 class PostsController < ApplicationController
-  def index
-    
-    @post = Post.new
-    @posts = Post.all
+  before_action :authenticate_user!
+  before_action :access_actions, only: [:index, :create]
 
-    
+  def index
   end
 
   def create
-    
-
-    @post = Post.new
-    @posts = Post.all
-
-   
-
     @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to posts_path
     else
+      flash.now[:notice] = 'Write a post!'
       render :index
     end
   end
 
   private
+
+  def access_actions
+    @post = Post.new
+    @posts = Post.all
+  end
 
   def post_params
     params.require(:post).permit(:body)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.where('id != ?', current_user)
+  end
+
+  def show
+  end
 end
