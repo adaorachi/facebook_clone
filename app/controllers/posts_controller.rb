8507587293f6e493ae255c3d728ff6f2f2e8bc53
FelipeEnne <felipeenne@gmail.com>
2020-01-15 14:19:22 -0300

    update the post model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,31 @@
 class PostsController < ApplicationController
   def index
+    
+    @post = Post.new
+    @posts = Post.all
+
+    
   end
 
-  def show
+  def create
+    
+
+    @post = Post.new
+    @posts = Post.all
+
+   
+
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
