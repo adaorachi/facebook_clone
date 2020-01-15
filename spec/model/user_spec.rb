5b560817073b91a5c814999f50399188d5cc22a0
FelipeEnne <felipeenne@gmail.com>
2020-01-15 14:36:15 -0300

    create rspec tests for user model

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe User, type: :model do
   context 'validations' do
     it { should validate_presence_of(:firstname) }
     it { should validate_presence_of(:surname) }
+    it { should validate_presence_of(:birthdate) }
+    it { should validate_presence_of(:gender) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 
   context 'Associations' do
-    it { should belong_to(:user) }
+    it { should have_many(:posts) }
   end
 end
